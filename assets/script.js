8a6b9b8b9217b72d1ad6192235500d9829572d3e
carlosflrs <carlos.flrsarlln@gmail.com>
2016-04-28 14:54:12 -0700

    a

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var index = 0;
 var startTop = 0;
-var startLeft = 0;
+var lastAdd = [0,0];
 
 var names = [];
 var texts = [];
@@ -27,30 +27,31 @@ function main() {
 }
 
 function addText() {
-    if (startTop > 550) {
+    if (startTop > 500) {
         i = index;
         clear();
         index = i;
     }
-    if (index < names.length) {
-        ran = getRandomInt(0, 2);
-        ran1 = getRandomInt(0, 6);
-        var obj = {name: names[index], text:texts[index]}
-        if (ran == 0 && index != 0 && index != 1) {
-            addRandomObject(obj);
-        } else {
-            addObject(obj);
-            startTop += 40;
-        }
-        index += 1
-        if (ran1 == )0 {
-            distract();
-        }
-    }
     if (index >= names.length) {
         index = 0;
     }
-    
+    if (index == 0) {
+        startTop = 0;
+    }
+    ran = getRandomInt(0, 3);
+    ran1 = getRandomInt(0, 5);
+    var obj = {name: names[index], text:texts[index]}
+    if (ran == 0 && index != 0 && index != 1) {
+        randomCount += 1;
+        addRandomObject(obj);
+    } else {
+        addObject(obj);
+        randomCount = 0;
+    }
+    index += 1
+    if (ran1 == 1 && ran != 0 && ran != 1) {
+        distract();
+    }
 }
 
 function removeText() {
@@ -58,18 +59,20 @@ function removeText() {
         index = index - 1;
         divName = names[index];
         $("#" + divName).remove();
-        startTop -= 40;
+        if (randomCount == 0) {
+            startTop -= 30;
+        } else {
+            randomCount -= 1;
+        }
     }
 }
 
 function addObject(obj) {
     var name = obj.name;
     var text = obj.text;
-    var t = startTop;
-    var l = startLeft;
-    var cr = Please.make_color();
     $("#text-box").append("<div id=" + name + ">" + text + "</div>");
-    $("#" + name).css({"margin-top":t, "margin-left":l, position:"absolute"});
+    $("#" + name).css({"margin-top":startTop, "margin-left":0, position:"absolute"});
+    startTop += 50;
 }
 
 function addRandomObject(obj) {
@@ -77,7 +80,9 @@ function addRandomObject(obj) {
     var text = obj.text;
     var t = getRandomInt(0, 500);
     var l = getRandomInt(0, 500);
-    var cr = Please.make_color();
+    colors = ["rgba(64, 153, 255, 1)", "rgba(59, 89, 152, 1)", "rgba(187, 0, 0, 1)", "rgba(18, 86, 136, 1)", "rgba(77, 194, 71, 1)", "rgba(255, 252, 0, 1)", "rgba(0, 191, 143, 1)"];
+    ran = getRandomInt(0, 6);
+    var cr = colors[ran];
     $("#text-box").append("<div id=" + name + ">" + text + "</div>");
     $("#" + name).css({"margin-top":t, "margin-left":l, position:"absolute", color:cr});
 }
@@ -92,9 +97,14 @@ function clear() {
 }
 
 function distract() {
+    culpables = ["your friends", "your girlfriend", "your boyfriend", "your mom", "your dad", "your brother", "your sister", "you", "us", "your goals"];
     colors = ["rgba(64, 153, 255, 0.7)", "rgba(59, 89, 152, 0.7)", "rgba(187, 0, 0, 0.7)", "rgba(18, 86, 136, 0.7)", "rgba(77, 194, 71, 0.7)", "rgba(255, 252, 0, 0.7)", "rgba(0, 191, 143, 0.7)"];
     ran = getRandomInt(0, 6);
     c = colors[ran];
+    ran = getRandomInt(0, 10);
+    $("#sponsored").text (function () {
+        return "Sponsored by " + culpables[ran] + ".";
+    });
     $("#ex").css({"background-color":c});
     $("#ex").show();
     $(".buttons").hide();
@@ -103,6 +113,27 @@ function distract() {
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
 }
 
 window.onload = main;
